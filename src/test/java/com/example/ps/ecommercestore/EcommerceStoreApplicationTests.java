ff2503ddf87261014e1051c29a3504e47745d2cc
CodeCommander7 <psppriyanshsharma454@gmail.com>
2026-04-21 15:19:18 +0530

    first commit

---
+++ src/test/java/com/example/ps/ecommercestore/EcommerceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ps.ecommercestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
